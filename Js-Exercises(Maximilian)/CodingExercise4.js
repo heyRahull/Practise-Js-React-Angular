9ac20f28eadfc4f90c63deec717748357378872f
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-05-05 00:43:53 +0530

    Create CodingExercise4.js

---
+++ Js-Exercises(Maximilian)/CodingExercise4.js
@@ -0,0 +1,26 @@
+// The built-in push method can be used to push new items onto an array - no matter if that arrays stores numbers, strings or any other kinds of values.
+
+// Therefore, as a first step, call numbers.push(8) to add the number 8 to the existing array:
+
+// let numbers = [10, 3, -1];
+// numbers.push(8);
+
+// As a second (and final) step, repeat this process with the number 2:
+
+// numbers.push(2);
+
+
+// Overall, the finished code should look like this:
+
+// let numbers = [10, 3, -1];
+// numbers.push(8);
+// numbers.push(2);
+
+let numbers = [10, 3, -1];
+
+function solve() {
+    // Todo: Your solution goes here
+    // Important: It must be put into this function!
+    numbers.push(8);
+    numbers.push(2);
+}
