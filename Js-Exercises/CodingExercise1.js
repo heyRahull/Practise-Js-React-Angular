3c74d639270c1024dec113c584cb84cb5bcf02e4
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-05-05 00:23:20 +0530

    Create CodingExercise1.js

---
+++ Js-Exercises/CodingExercise1.js
@@ -0,0 +1,13 @@
+// Exercise: Variables & Operators
+// To practice what you learned, complete the following task:
+
+// Create two variable or constants, a and b, that should store the values 5 and 3 respectively. Thereafter, also add a third variable or constant, sum, that stores the sum of a and b.
+
+// You don't have to do anything else with those variables / constants.
+
+// Important: For the automatic solution checking to work, make sure you use exactly the variable / constant names a, b and sum!
+
+// Todo: Your solution code goes here ...
+const a = 5;
+const b = 3;
+const sum = a+b;
