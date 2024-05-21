4473d3ea0c5df427f0649daa893ad855ab6b1914
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-05-21 23:59:58 +0530

    Update CodingExercise3.js

--- Js-Exercise(Jonas)/CodingExercise3.js
+++ Js-Exercise(Jonas)/CodingExercise3.js
@@ -2,7 +2,7 @@
 // There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
 
 // Your tasks ::
-
+j
 // 1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
 
 // 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
